7a61a2aa0a21bfb388e5d2d4aefab07141bac2c2
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-16 03:23:35 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,7 +1,3 @@
 function solution(n, k) {
-    let drink = k;
-    if(k !== 0){
-        k -= Math.floor(n/10);
-    }
-    return n*12000 + k*2000;
+    return n*12000 + ((k - ~~(n/10))) * 2000;
 }
