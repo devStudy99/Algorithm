377491aed36496c4dedef70d6de1f4c3d90f87b7
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-16 05:17:37 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,33 +1,44 @@
 function solution(age) {
     
-    let alphabet = '';
+//     let alphabet = '';
     
-    let ageArr = age.toString().split(''); 
+//     let ageArr = age.toString().split(''); 
     
-    let arrLength = ageArr.length;
+//     let arrLength = ageArr.length;
     
-    const alObj = {
-        0: "a",
-        1: "b",
-        2: "c",
-        3: "d",
-        4: "e",
-        5: "f",
-        6: "g",
-        7: "h",
-        8: "i",
-        9: "j"
-    }
+//     const alObj = {
+//         0: "a",
+//         1: "b",
+//         2: "c",
+//         3: "d",
+//         4: "e",
+//         5: "f",
+//         6: "g",
+//         7: "h",
+//         8: "i",
+//         9: "j"
+//     }
     
-    for(let i=0; i<arrLength; i++){
-        console.log(i);
-        for(const key in alObj){
-         if(key == ageArr[0]){
-             alphabet += alObj[key];
-             ageArr.shift();
-         }   
-        }
-    }
+//     for(let i=0; i<arrLength; i++){
+//         console.log(i);
+//         for(const key in alObj){
+//          if(key == ageArr[0]){
+//              alphabet += alObj[key];
+//              ageArr.shift();
+//          }   
+//         }
+//     }
 
-    return alphabet;
+//     return alphabet;
+    
+    let answer = '';
+    
+    const stringAge = String(age);
+    
+    let cnt = 0;
+    while(cnt < stringAge.length){
+        answer = answer + String.fromCharCode(stringAge[cnt].charCodeAt(0) + 49);
+        cnt++;
+    }
+    return answer;
 }
