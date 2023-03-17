806dcb2b704084dab799b4e3425fb6916889c696
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-17 12:09:20 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,10 +1,15 @@
 function solution(numbers, direction) {
-    if(direction === "right"){
-        numbers.unshift(numbers[numbers.length-1]);
-        numbers.pop();
-    } else {
-        numbers.push(numbers[0]);
-        numbers.shift();
-    }
+    // if(direction === "right"){
+    //     numbers.unshift(numbers[numbers.length-1]);
+    //     numbers.pop();
+    // } else {
+    //     numbers.push(numbers[0]);
+    //     numbers.shift();
+    // }
+    
+    // 마지막 요소 제거한걸 첫 요소로 추가
+    // 첫 요소 제거한걸 마지막 요소로 추가
+    direction === 'right' ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift());
+
     return numbers;
 }
