fdb997687fdf6dea6d1a48b3d37df5440df4b0d3
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-17 12:57:38 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -11,6 +11,8 @@ function solution(num_list, n) {
     // return result;
     let answer = [];
 
+    // 배열의 길이가 0이 될 때가지 반복문 돌려야 함.
+    // while 조건문에 배열의 길이를 넣으면 0이 될 때 반복문이 종료됨.
     while(num_list.length) {
         answer.push(num_list.splice(0,n));
     }
