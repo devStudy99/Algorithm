7c2c16e85425e120929328540a144a07a3c6cf5d
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-17 12:51:16 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,13 +1,20 @@
 function solution(num_list, n) {
-    let result = [];
-    let tempArray = [];
-    for(let i=0; i<num_list.length; i++){
-        tempArray.push(num_list[i]);
-        if(tempArray.length === n){
-            result.push(tempArray);
-            tempArray = [];
-        }
+    // let result = [];
+    // let tempArray = [];
+    // for(let i=0; i<num_list.length; i++){
+    //     tempArray.push(num_list[i]);
+    //     if(tempArray.length === n){
+    //         result.push(tempArray);
+    //         tempArray = [];
+    //     }
+    // }
+    // return result;
+    let answer = [];
+
+    while(num_list.length) {
+        answer.push(num_list.splice(0,n));
     }
-    return result;
+
+    return answer;
 }
 
