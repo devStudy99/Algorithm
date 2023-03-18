57f096aa46beb8a5b1e908ca39886738633178eb
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-19 04:50:40 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,11 @@
+function solution(my_string, num1, num2) {
+    const first = my_string[num1];
+    const second = my_string[num2];
+    
+    let answer = my_string.split('');
+    
+    answer.splice(num1, 1, second);
+    answer.splice(num2, 1, first);
+    
+    return answer.join('');
+}
