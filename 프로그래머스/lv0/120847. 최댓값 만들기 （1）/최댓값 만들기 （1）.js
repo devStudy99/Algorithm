47aa03987f4a52a2c90257fb5f1c7c548d77a806
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-18 12:12:24 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,11 +1,18 @@
 function solution(numbers) {
-    let max = 0;
-    numbers.map((number, i)=>{
-        for(let j=0; j<numbers.length; j++){
-            if(i !== j && max <= number*numbers[j]){
-                max = number*numbers[j];
-            }
-        }
-    })
-    return max;
+    
+//     let max = 0;
+    
+//     numbers.map((first, i)=>{
+//         numbers.map((second, j)=>{
+//             if(i !== j && max <= first*second){
+//                 max = first * second;
+//             }
+//         })
+//     })
+    
+//     return max;
+    
+    numbers.sort((a,b)=>b-a);
+    
+    return numbers[0]*numbers[1];
 }
