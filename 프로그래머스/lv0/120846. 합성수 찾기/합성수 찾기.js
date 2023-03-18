a4923358f0c5560e04d3f7fa19d3a5209cefa823
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-18 11:48:10 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let cnt = 0;
+    let answer = 0;
+    for(let i=1; i<=n; i++){
+        cnt = 0;
+        for(let j=1; j<=i; j++){
+            if(i%j === 0){
+                cnt++;
+            }
+            if(cnt === 3){
+                answer++;
+                break;
+            }
+        }
+    }
+    return answer;
+}
