a92a9d341b224933cf926c5a7863f433deee0ced
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-19 02:42:56 +0900

    [level 0] Title: 369게임, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,9 +1,10 @@
 function solution(order) {
-    let cnt = 0;
-    String(order).split('').map(num=>{
-        if(Number(num) !== 0 && Number(num)%3 === 0){
-            cnt++;
-        }
-    });
-    return cnt;
+//     let cnt = 0;
+//     String(order).split('').map(num=>{
+//         if(Number(num) !== 0 && Number(num)%3 === 0){
+//             cnt++;
+//         }
+//     });
+//     return cnt;
+    return (order+'').replace(/[0, 1, 2, 4, 5, 7, 8]/g, '').length;
 }
