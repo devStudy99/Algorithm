0e7bb54f496d2a705d54cfe11f8b3c9b7daab18d
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-19 02:02:15 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,10 +1,9 @@
 function solution(array, n) {
     array.push(n);
     array.sort((a,b)=>a-b);
+    
     let before = array.indexOf(n)-1;
     let after = array.indexOf(n)+1;
-    if(n-array[before]>array[after]-n){
-        return array[after];
-    }
-    return array[before];
+    
+    return n-array[before]>array[after]-n ? array[after] : array[before];
 }
