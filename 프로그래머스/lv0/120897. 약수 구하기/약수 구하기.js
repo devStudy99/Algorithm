62b5604a3c45b5dca6e7d1d9dd410f40c3a10102
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-19 05:04:05 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,11 +1,15 @@
 function solution(n) {
-    let answer = [];
+//     let answer = [];
     
-    for(let i=1; i<=n; i++){
-       if(n%i===0){
-           answer.push(i);
-       } 
-    }
+//     for(let i=1; i<=n; i++){
+//        if(n%i===0){
+//            answer.push(i);
+//        } 
+//     }
     
-    return answer;
+//     return answer;
+    
+    // n개의 빈배열을 0값으로 채운 뒤 n으로 나눴을 때 나머지 0인 값들만 남도록
+    return Array(n).fill(0).map((v, index) => index+1).filter((v) => n%v===0);
+    
 }
