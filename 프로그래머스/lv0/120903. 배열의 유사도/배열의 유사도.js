3bfb4a392142a6dee600d8e3d0a521f1281b4893
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-19 19:24:52 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,10 +1,13 @@
 function solution(s1, s2) {
-    let cnt = 0;
-    s1.map(val => {
-        if(s2.indexOf(val) !== -1){
-            cnt++;
-        }
-    })
-    return cnt;
+    // let cnt = 0;
+    // s1.map(v => {
+    //     if(s2.indexOf(v) !== -1){
+    //         cnt++;
+    //     }
+    // })
+    // return cnt;
+    
+    const arr = s1.filter((v) => s2.includes(v));
+    return arr.length;
 
 }
