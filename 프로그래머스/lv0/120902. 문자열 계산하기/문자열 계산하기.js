6fbdbdd2c6f9314e225a3333c6b23dad0104604a
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-19 21:42:45 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -3,12 +3,7 @@ function solution(my_string) {
     let sum = Number(arr[0]);
 
     for(let i=1; i<arr.length; i+=2){
-        console.log(i);
-        if(arr[i]==="+"){
-            sum += Number(arr[i+1]);
-        }else{
-            sum -= Number(arr[i+1]);
-        }
+        arr[i] === "+" ? sum += Number(arr[i+1]) : sum -= Number(arr[i+1]);
     }
     
     return sum;
