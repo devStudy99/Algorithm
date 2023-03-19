c3a3e4788ed38ed5a2137652748f4c8f747059eb
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-19 22:20:07 +0900

    [level 0] Title: OX퀴즈, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,26 @@
+function solution(quiz) {
+    let temp = [];
+    let answer = [];
+    
+    quiz.map(v=>{
+        
+        temp = v.split(" ");
+        
+        if(temp[1] === "+"){
+            if(Number(temp[0]) + Number(temp[2]) === Number(temp[4])){
+                answer.push("O");
+            }else{
+                answer.push("X");
+            }
+        } else {
+            if(Number(temp[0]) - Number(temp[2]) === Number(temp[4])){
+                answer.push("O");
+            }else{
+                answer.push("X");
+            }
+        }        
+        
+        temp = [];
+    })
+    return answer;
+}
