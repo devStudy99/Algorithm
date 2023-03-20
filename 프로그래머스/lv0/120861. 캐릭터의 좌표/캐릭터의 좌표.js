b0a8d7639ef53c749dc195ad130d833fb35433a1
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-20 21:49:06 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,44 +1,58 @@
 function solution(keyinput, board) {
-    let xLength = ~~(board[0]/2);
-    let yLength = ~~(board[1]/2);
+//     let xLength = board[0]/2-1;
+//     let yLength = board[1]/2-1;
     
-    let xCnt = 0;
-    let yCnt = 0;
+//     let xCnt = 0;
+//     let yCnt = 0;
     
     
-    keyinput.map(v => {
-        if(v === "up" || v === "down"){
-            if(Math.abs(yCnt) < yLength){
-                v === "up" ? yCnt++ : yCnt--;
-            } else if(yCnt === yLength && v === "down"){
-                yCnt--;
-            } else if(yCnt === -yLength && v === "up"){
-                yCnt++;
-            }
-        } else if(v === "right" || v === "left"){
-            if(Math.abs(xCnt) < xLength){
-                v === "right" ? xCnt++ : xCnt--;
-            } else if(xCnt === xLength && v === "left"){
-                xCnt--;
-            } else if(xCnt === -xLength && v === "right"){
-                xCnt++;
-            }
+//     keyinput.map(v => {
+//         if(v === "up" || v === "down"){
+//             if(Math.abs(yCnt) < yLength){
+//                 v === "up" ? yCnt++ : yCnt--;
+//             } else if(yCnt === yLength && v === "down"){
+//                 yCnt--;
+//             } else if(yCnt === -yLength && v === "up"){
+//                 yCnt++;
+//             }
+//         } else if(v === "right" || v === "left"){
+//             if(Math.abs(xCnt) < xLength){
+//                 v === "right" ? xCnt++ : xCnt--;
+//             } else if(xCnt === xLength && v === "left"){
+//                 xCnt--;
+//             } else if(xCnt === -xLength && v === "right"){
+//                 xCnt++;
+//             }
+//         }
+//     })
+    
+//     function xyCnt(cnt, length){
+//         if(cnt < 0){
+//             if(Math.abs(cnt) > length){
+//                 cnt = - + length;
+//             }
+//         } else if(cnt > 0){
+//             if(cnt > length){
+//                 cnt = length;
+//             }
+//         }
+//         return cnt;
+//     }
+
+//     return [xyCnt(xCnt, xLength), xyCnt(yCnt, yLength)];
+    
+    let answer = [0,0];
+    
+    // left와 down일 때는 -할거니까 -인 상태로 비교해야 됨
+    keyinput.map(v=>{
+        switch(v){
+            case 'left': if (-answer[0] < board[0]/2-1) answer[0]--; break;
+            case 'right': if (answer[0] < board[0]/2-1) answer[0]++; break;
+            case 'up': if (answer[1] < board[1]/2-1) answer[1]++; break;
+            case 'down': if (-answer[1] < board[1]/2-1) answer[1]--; break;
         }
     })
-    
-    function xyCnt(cnt, length){
-        if(cnt < 0){
-            if(Math.abs(cnt) > length){
-                cnt = - + length;
-            }
-        } else if(cnt > 0){
-            if(cnt > length){
-                cnt = length;
-            }
-        }
-        return cnt;
-    }
 
-    return [xyCnt(xCnt, xLength), xyCnt(yCnt, yLength)];
+    return answer;
     
 }
