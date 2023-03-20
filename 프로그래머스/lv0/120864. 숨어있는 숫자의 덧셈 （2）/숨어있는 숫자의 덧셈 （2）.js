5b261823f70e31ee7622b32239719d5ea277666f
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-20 23:56:23 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,9 +1,4 @@
 function solution(my_string) {
-    
-    const arr = my_string.replaceAll(/[a-zA-z]/g, " ").split(" ");
-    
-    return arr.filter(v=>Number(v) > 0).reduce((acc, cur)=>Number(acc)+Number(cur),0);
-
-
-    
+    var answer = 0;
+    return answer;
 }
