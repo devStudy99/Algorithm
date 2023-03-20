0b474e2601ae1b6e1da91f6f41e9a2a308be3232
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-20 14:52:34 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,26 +1,34 @@
 function solution(my_str, n) {
-    let strArr = my_str.split('');
-    let length = strArr.length;
-    let str = "";
-    let cnt = 0;
-    let answer = [];
+//     let strArr = my_str.split('');
+//     let length = strArr.length;
+//     let str = "";
+//     let cnt = 0;
+//     let answer = [];
 
-    while(strArr.length){
-        if(cnt===n){
-            answer.push(str);
-            str = "";
-            cnt = 0;
-        }
-        if(strArr.length === length%n){
-            answer.push(strArr.join(''));
-            return answer;
-        }
-        str += strArr[0];
-        if(strArr.length === 1){
-            answer.push(str);
-            return answer;
-        }
-        strArr.shift();
-        cnt++;        
+//     while(strArr.length){
+//         if(cnt===n){
+//             answer.push(str);
+//             str = "";
+//             cnt = 0;
+//         }
+//         if(strArr.length === length%n){
+//             answer.push(strArr.join(''));
+//             return answer;
+//         }
+//         str += strArr[0];
+//         if(strArr.length === 1){
+//             answer.push(str);
+//             return answer;
+//         }
+//         strArr.shift();
+//         cnt++;        
+//     }
+    
+    let string = my_str.split("");
+    let answer = [];
+    while(string.length > n){
+        answer.push(string.splice(0,n).join(""));
     }
+    answer.push(string.join(""));
+    return answer;
 }
