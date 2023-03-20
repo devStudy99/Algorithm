33ed356bc2072a54ff89ebc884b6a32e557869ca
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-20 14:14:19 +0900

    [level 0] Title: 7의 개수, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,11 +1,12 @@
 function solution(array) {
-    let cnt = 0;
-    array.map(v1=>{
-        String(v1).split('').map(v2=>{
-         if(Number(v2)===7){
-             cnt++;
-         }   
-        })
-    })
-    return cnt;
+    // let cnt = 0;
+    // array.map(v1=>{
+    //     String(v1).split('').map(v2=>{
+    //      if(Number(v2)===7){
+    //          cnt++;
+    //      }   
+    //     })
+    // })
+    // return cnt;
+    return array.join('').split('').filter(v=>Number(v)===7).length;
 }
