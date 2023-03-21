af7209530c85461c2e2cba60b4437dae6ddc0b3b
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-22 02:35:55 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,19 +1,24 @@
 function solution(n) {
-    let str = "";
+    
+//     let str = "";
 
-    while(n>0){
-        str += n%3;
-        n = ~~(n/3);
-    }
+//     while(n>0){
+//         str += n%3;
+//         n = ~~(n/3);
+//     }
     
-    let cnt = 0;
-    let sum = 0;
+//     let cnt = 0;
+//     let sum = 0;
     
-    while(cnt<str.length){
-        sum += str[str.length-1-cnt] * 3 ** cnt;
-        cnt++;
-    }
+//     while(cnt<str.length){
+//         sum += str[str.length-1-cnt] * 3 ** cnt;
+//         cnt++;
+//     }
     
-    return sum;
+//     console.log(parseInt([...n.toString(3)].reverse().join(""), 3));
+    
+//     return sum;
+
+    return parseInt([...n.toString(3)].reverse().join(""), 3);
 
 }
