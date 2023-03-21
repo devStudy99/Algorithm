b3d06e7b3da434ef4d1906f9a0880aecaa7b1943
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-22 02:27:40 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    let str = "";
+
+    while(n>0){
+        str += n%3;
+        n = ~~(n/3);
+    }
+    
+    let cnt = 0;
+    let sum = 0;
+    
+    while(cnt<str.length){
+        sum += str[str.length-1-cnt] * 3 ** cnt;
+        cnt++;
+    }
+    
+    return sum;
+
+}
