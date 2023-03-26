7a2fd64273f2ba1803d9d081cbc82036799b182a
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-27 02:56:24 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.96 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,36 +1,17 @@
 function solution(number, limit, power) {
     
-    let divisor = [];
-    let cnt = 0;
-    let answer = 0;
-    let lastDivisor = 0;
+    var answer = 0;
     
-    for(let i=1; i<=number; i++){
-        for(let j=1; j<=i; j++){
-            if(i%j===0){
-                if(lastDivisor === j){
-                    break;
-                }
-                lastDivisor = i/j;
-                // 소수인 경우와 아닌 경우
-                if(j*j===i){
-                    cnt += 1;
-                    break;
-                }else{
-                    cnt += 2;   
-                }
-            }
+    for (let i = 1; i <= number; i++){
+        let count = 0;
+        // 제곱값까지만 구해도 됨
+        for (let j = 1; j * j <= i; j++){
+            if (j * j == i) count++;
+            else if (i % j == 0) count += 2;
         }
-        divisor.push(cnt);
-        cnt = 0;
-        lastDivisor = 0;
+        if (count > limit) count = power;
+        answer += count;
     }
     
-    divisor.forEach(v=>{
-        if(v>limit) answer += power;
-        if(v<=limit) answer += v;
-    })
-    
     return answer;
-    
 }
