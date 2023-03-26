b523f8b8030f91e8c57a9c466e2547caa673ac2f
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-27 00:16:38 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,22 +1,22 @@
 function solution(strings, n) {
    
-//     strings.sort((a,b)=>{
+    strings.sort((a,b)=>{
         
-//         // 만약 a[n]이랑 b[n]이랑 같으면 사전순 같지 않을 경우 n번째 글자 기준 사전순    
-//         if(a[n] === b[n]){
-//             if(a > b) return 1;
-//             if(a < b) return -1;
-//             return 0;
-//         }
+        // 만약 a[n]이랑 b[n]이랑 같으면 사전순 같지 않을 경우 n번째 글자 기준 사전순    
+        if(a[n] === b[n]){
+            if(a > b) return 1;
+            if(a < b) return -1;
+            return 0;
+        }
         
-//         return a[n].charCodeAt(0) - b[n].charCodeAt(0);
+        return a[n].charCodeAt(0) - b[n].charCodeAt(0);
         
-//     })
+    })
     
-//     return strings;
+    return strings;
 
-    // a.localeCompare(b) => 오름차순 / b.localeCompare(a) => 내림차순 
-    return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
+    // a.localeCompare(b) => 오름차순 / b.localeCompare(a) => 내림차순 -> 속도 느림
+    // return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
 
     
 }
