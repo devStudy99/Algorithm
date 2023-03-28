6dc01ba319ad06237db645cc50d0326b06cc6e7b
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-28 13:24:47 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,24 +1,26 @@
 function solution(n, arr1, arr2) {
-    const temp1 = arr1.map(v=>{
-        return v.toString(2).padStart(n, 0);
-    })
-    const temp2 = arr2.map(v=>{
-        return v.toString(2).padStart(n, 0);
-    })
+//     const temp1 = arr1.map(v=>{
+//         return v.toString(2).padStart(n, 0);
+//     })
+//     const temp2 = arr2.map(v=>{
+//         return v.toString(2).padStart(n, 0);
+//     })
 
-    let answer = [];
-    let str = "";
+//     let answer = [];
+//     let str = "";
     
-    for(let i=0; i<temp1.length; i++){
-        str += Number(temp1[i])+Number(temp2[i]);
-        answer.push(str);
-        str = "";
-    }
+//     for(let i=0; i<temp1.length; i++){
+//         str += Number(temp1[i])+Number(temp2[i]);
+//         answer.push(str);
+//         str = "";
+//     }
     
-    answer.filter((v, i) =>{
-        answer[i] = v.padStart(n, 0).replace(/0/g, ' ');
-        answer[i] = answer[i].replace(/[1-9]/g, '#');
-    })
+//     answer.filter((v, i) =>{
+//         answer[i] = v.padStart(n, 0).replace(/0/g, ' ');
+//         answer[i] = answer[i].replace(/[1-9]/g, '#');
+//     })
     
-    return answer;
+//     return answer;
+    
+    return arr1.map((a,i)=>(a|arr2[i]).toString(2).padStart(n,0).replace(/0/g,' ').replace(/1/g,'#'))
 }
