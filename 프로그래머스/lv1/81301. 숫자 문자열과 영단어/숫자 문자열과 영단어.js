e33a732a7505092fe6cf9f04685ddea18c62b60e
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-28 14:00:20 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,33 +1,42 @@
 function solution(s) {
-    const word = {
-        zero: 0,
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9
-    }
+//     const word = {
+//         zero: 0,
+//         one: 1,
+//         two: 2,
+//         three: 3,
+//         four: 4,
+//         five: 5,
+//         six: 6,
+//         seven: 7,
+//         eight: 8,
+//         nine: 9
+//     }
     
-    let str = "";
-    let answer = "";
+//     let str = "";
+//     let answer = "";
     
-    for(let i=0; i<s.length; i++){
-        // 숫자면 바로 추가
-        if((/\d/).test(s[i])){
-            answer += s[i];
-        } else{
-            str += s[i];
-            // 해당하는 값이 있으면 추가
-            if(word[str] !== undefined){
-                answer += word[str];
-                str = "";
-            }
-        }
-    }
+//     for(let i=0; i<s.length; i++){
+//         // 숫자면 바로 추가
+//         if((/\d/).test(s[i])){
+//             answer += s[i];
+//         } else{
+//             str += s[i];
+//             // 해당하는 값이 있으면 추가
+//             if(word[str] !== undefined){
+//                 answer += word[str];
+//                 str = "";
+//             }
+//         }
+//     }
     
-    return +answer;
+//     return +answer;
+    
+    const word = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    for(let i=0; i<=9; i++){
+        const regex = new RegExp(`${word[i]}`, 'g');
+        s = s.replace(regex, i);
+    }    
+
+    return +s;
 }
