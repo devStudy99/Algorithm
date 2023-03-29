f280ce3f549242d4dbdff4ee75f4c258057da794
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-30 06:23:49 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,25 +1,13 @@
 function solution(brown, yellow) {
-    
-    let arr = [];
-    let answer = [];
-    
+
     for(let i=3; i<=brown/2; i++){
         
-        let width = 0;
-        let height = 0;
+        let height = (brown+yellow)/i;
+        let width = (brown+yellow)/height;
         
-        if((brown+yellow)%i===0){
-            height = (brown+yellow)/i;
-            width = (brown+yellow)/height;
+        if(width >= height){
+            if((width-2)*(height-2)===yellow) return [width, height];
         }
-        if(width !== 0 && height !== 0 && width >= height){
-            arr.push([width, height]);
-        }
+        
     }
-    
-    arr.forEach(v=>{
-        if((v[0]-2) * (v[1]-2) === yellow) answer = v;
-    })
-   
-    return answer;
 }
