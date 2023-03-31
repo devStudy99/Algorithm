30db4fe09f7a005e776dba68384642ba13e856b9
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-04-01 02:26:14 +0900

    [unrated] Title: 덧칠하기, Time: 5.13 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,12 +1,11 @@
 function solution(n, m, section) {
     
     let wall = new Array(n).fill(true);
+    let answer = 0;
     
     section.forEach(v=>{
         wall[v-1] = false;
     })
-    
-    let answer = 0;
     
     for(let i=0; i<wall.length; i++){
         if(wall[i] === false){
