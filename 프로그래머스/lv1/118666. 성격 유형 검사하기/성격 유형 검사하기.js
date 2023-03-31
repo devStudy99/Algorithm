623dc3a7e342d512b4a878e0f2d87743cf047755
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-03-31 16:18:25 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,38 +1,24 @@
 function solution(survey, choices) {
     
-    let personality = ["R", "T", "C", "F", "J", "M", "A", "N"];
-    let sumArr = [0, 0, 0, 0, 0, 0, 0, 0];
+    const personality = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 }
     let point = [3, 2, 1, 0, 1, 2, 3];
     
     survey.forEach((v, i)=>{
-        // 1~3까지는 0번째
-        if(choices[i] < 4){
-            // a번째 인덱스와 매칭되는 b인덱스에 점수 추가
-            sumArr[personality.indexOf(v[0])] += point[choices[i]-1];
-        }
-        // 5~7까지는 1번째
-        if(choices[i] > 4){
-            // a번째 인덱스와 매칭되는 b인덱스에 점수 추가
-            sumArr[personality.indexOf(v[1])] += point[choices[i]-1];
-        }
+        if(choices[i] < 4) personality[v[0]] += point[choices[i]-1];
+        if(choices[i] > 4) personality[v[1]] += point[choices[i]-1];
     })
+    
+    const arr = Object.entries(personality);
 
-    const result = sumArr.reduce((acc, cur, i) => {
-      if (i % 2 === 0) {
-        acc.push([cur, sumArr[i + 1]]);
-      }
-      return acc;
-    }, []);
-    
-    let answer = "";
-    let pi = 0;
-    
-    result.forEach((v, i)=>{
-        if(v[0] > v[1]) answer += personality[pi];
-        if(v[0] < v[1]) answer += personality[pi+1];
-        if(v[0] === v[1]) answer += personality[pi];
-        pi += 2;
-    })
+    const answer = arr.reduce((acc, cur, i) =>{
+        if(i%2 === 0){
+            if(cur[1] > arr[i+1][1]) acc += cur[0];
+            if(cur[1] < arr[i+1][1]) acc += arr[i+1][0];
+            if(cur[1] === arr[i+1][1]) acc += cur[0];
+        }
+        return acc;
+    }, "")
     
     return answer;
+
 }
