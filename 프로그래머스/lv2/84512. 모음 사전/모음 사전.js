12e45e5f36c6de500a45097aa45bf8fa9531c5b4
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-04-17 00:09:42 +0900

    [level 2] Title: 모음 사전, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,26 @@
+function solution(word) {
+    let dic = ["A","E","I","O","U"];
+
+    let ChangeSwitcher = true
+    let answer = -1;
+
+    function DFS(W){
+        if(ChangeSwitcher){
+            answer++;
+            if(W === word){
+                ChangeSwitcher = false;
+                return ;
+            } else if (W !== word && W.length < 5){
+                for (let i = 0; i < dic.length; i++) {
+                    DFS(W+dic[i]);                
+                }
+            } else {
+                return;
+            }
+        }
+    }
+
+    DFS("");
+
+    return answer;
+}
