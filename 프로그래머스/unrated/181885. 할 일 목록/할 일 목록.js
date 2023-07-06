e9941d001eeeabc1cf1649d804b39bd3d83696ef
devStudy99 <127740794+devStudy99@users.noreply.github.com>
2023-07-06 23:05:41 +0900

    [unrated] Title: 할 일 목록, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,9 @@
+function solution(todo_list, finished) {
+  const unfinishedTasks = [];
+  for (let i = 0; i < todo_list.length; i++) {
+    if (!finished[i]) {
+      unfinishedTasks.push(todo_list[i]);
+    }
+  }
+  return unfinishedTasks;
+}
